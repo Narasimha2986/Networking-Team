05f75684d11bece182628cb002232e82577799fb
Narasimha2986 <114830385+Narasimha2986@users.noreply.github.com>
2022-10-04 23:27:57 +0530

    Update Task.py

--- pavan/Task.py
+++ pavan/Task.py
@@ -17,4 +17,3 @@ print("Top two titles with most number of comments")
 print("*******************************************")
 for rec in sorted_records[0:2]:
     print(rec["title"] + "-" +  str(rec["num_comments"]))
-pavan
